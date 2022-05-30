d4cd4276b337aff2a7601b8c2bb1fdc597645d9a
Prishal <92685854+Prishal-git@users.noreply.github.com>
2022-05-30 14:34:20 +0100

    inital commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from werkzeug import exceptions
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(exceptions.NotFound)
+def page_not_found(err):
+    return render_template("404.html", error=err), 404
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
+app.run()
